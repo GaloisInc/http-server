bc98d4c494512be427c22908acecfb24539049ba
Brian Lewis <brian@lorf.org>
2009-06-13 00:28:33 -0500

    Minor hlint cleanup.

--- Network/HTTP/Server/Response.hs
+++ Network/HTTP/Server/Response.hs
@@ -32,7 +32,7 @@ data StatusCode
 -- Intended to be used for (bad) erros.
 -- Adds a "close" header.
 err_response :: BufferType a => StatusCode -> Response a
-err_response code = insertHeader HdrConnection "close" (respond code)
+err_response = insertHeader HdrConnection "close" . respond
 
 -- | Make a simple response with the given staus and body.
 -- No headers or body.
